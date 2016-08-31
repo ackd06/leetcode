ebe2f2f43b63408fb474a605ff285da3f82f8a3d
trigkit <312905679@qq.com>
2016-08-31 23:16:46 +0800

    leetcode 第一天

---
+++ leetcode/src/zsz/develop/MainEntry.java
@@ -0,0 +1,12 @@
+package zsz.develop;
+
+public class MainEntry {
+
+	public static void main(String[] args) {
+		ReverseStringSolution reverseStringSolution = new ReverseStringSolution();
+		String reverseStringMyOne = reverseStringSolution.reverseStringOtherTwo("hello");
+		System.out.println(reverseStringMyOne);
+
+	}
+
+}
